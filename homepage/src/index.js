328ce4dd78f114a57f355eb33904575c366eca01
dudgns2824 <dudgns2824@gmail.com>
2024-02-01 18:55:51 +0900

    외부 템플릿 적용 중

--- homepage/src/index.js
+++ homepage/src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    Link
+} from "react-router-dom"
+import "./assets/dashboard-react.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Layout from "./layouts/AuthLayouts"
+import Login from "./views/Login"
+import Register from "./views/Register"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/auth" element={<Layout/>}>
+        <Route path="login" element={<Login/>}/>
+        <Route path="register" element={<Register/>}/>
+    </Route>
+
+))
+
+function App() {
+    return (
+        <RouterProvider router={router} />
+    )
+}
+
+ReactDOM
+    .createRoot(document.getElementById('root'))
+    .render(<App />);
---
+++ homepage/src/router/AppRouter.jsx
@@ -0,0 +1,6 @@
+export default {
+    // Roots
+    home: "/",
+    login: "/login",
+    register: "/register",
+};
