58a41d497d5f688e3db1ee7edda5f839a74a4eda
dudgns2824 <soar0531@cknb.co.kr>
2024-04-25 14:30:37 +0900

    header footer 기본 내용

--- homepage/src/layouts/common/DefautLayout.vue
+++ homepage/src/layouts/common/DefautLayout.vue
@@ -1,8 +1,23 @@
 <script setup lang="ts">
+import Header from "@/layouts/common/Header.vue";
+import Footer from "@/layouts/common/Footer.vue";
 </script>
 
 <template>
-<div>test</div>
+  <v-locale-provider>
+    <v-app :class="[]">
+      <Header></Header>
+
+      <v-main class="page-wrapper">
+        <v-container>
+          <RouterView></RouterView>
+        </v-container>
+        <v-container>
+          <Footer></Footer>
+        </v-container>
+      </v-main>
+    </v-app>
+  </v-locale-provider>
 </template>
 
 <style scoped>
